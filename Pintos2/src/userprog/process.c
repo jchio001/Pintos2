65a1662255a75236d91a2bc7857f217c100bfda8
Jonathan Chiou <jchio001@sledge.cs.ucr.edu>
2015-03-14 18:24:55 -0700

    Housekeeping

--- Pintos2/src/userprog/process.c
+++ Pintos2/src/userprog/process.c
@@ -106,7 +106,7 @@ start_process (void *file_name_) {
    This function will be implemented in problem 2-2.  For now, it
    does nothing. */
 int
-process_wait (tid_t child_tid UNUSED) {
+process_wait (tid_t child_tid) {
   struct child_process* cp = get_child_process(child_tid);
   if (!cp || cp->wait)    
       return -1;
